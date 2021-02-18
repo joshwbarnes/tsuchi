391868f845ed57c7f663f4a4e2887b5705519026
Josh <joshuawilliam.barnes@gmail.com>
2021-02-18 21:34:32 +1100

    setting up heroku

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :list
 
-  validates [:name, :item_category], presence: true
+  validates :name, presence: true
+  validates :item_category, presence: true
 end
