13977cd37c0f1e4d09df4ee21c9e6ded82a3a1db
Aya Tsurusaki <tsurusaki.yan@gmail.com>
2021-02-20 17:01:49 +1100

    changed controller and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :mobile_number, :address])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:fist_name, :mobile_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [])
   end
 end
