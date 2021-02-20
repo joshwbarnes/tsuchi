06bdf486bdc8e7be6bd336488240850ddc8e22a5
Lucie Roland <lucieemroland@gmail.com>
2021-02-20 15:58:12 +1100

    created the new and edit a shopping list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,24 +1,37 @@
 class ListsController < ApplicationController
 
-  authorize @list
-
   def index
-    @lists = List.all
+    @lists = policy_scope(List).order(created_at: :desc)
   end
 
   def show
+     authorize @list
   end
 
   def new
+    @list = List.new
+     authorize @list
   end
 
   def create
+    @list = List.new(list_params)
+     authorize @list
+    if @list.save
+    redirect_to list_path(@list)
+    else
+    render :new
+    end
   end
 
   def edit
+    @list = find_list
+    authorize @list
   end
 
   def update
+    @list = find_list
+    authorize @list
+    @list.update(list_params)
   end
 
   def delete
--- app/policies/list_policy.rb
+++ app/policies/list_policy.rb
@@ -1,16 +1,17 @@
 class ListPolicy < ApplicationPolicy
-  # class Scope < Scope
-  #   def resolve
-  #     scope.all
-  #   end
-  # end
-
-  def index?
-    return true
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
   end
 
   def create?
     return true
+  end
+
+
+  def update?
+    record.user == user
   end
 
 end
