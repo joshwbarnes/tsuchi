263f6bde9df0e5884ba96d2e2a14978d09138c1e
Lucie Roland <lucieemroland@gmail.com>
2021-02-23 19:56:44 +1100

    fixed pundit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,48 +1,48 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update, :delete]
+
   def index
     @lists = policy_scope(List).order(created_at: :desc)
   end
 
   def show
-     authorize @list
-  end
+   authorize @list
+ end
 
-  def new
-    @list = List.new
-     authorize @list
-  end
+ def new
+  @list = List.new
+  authorize @list
+end
 
-  def create
-    @list = List.new(list_params)
-     authorize @list
-    if @list.save
+def create
+  @list = List.new(list_params)
+  authorize @list
+  if @list.save
     redirect_to list_path(@list)
-    else
+  else
     render :new
-    end
   end
+end
 
-  def edit
-    @list = find_list
-    authorize @list
-  end
+def edit
+  authorize @list
+end
 
-  def update
-    @list = find_list
-    authorize @list
-    @list.update(list_params)
-  end
+def update
+  authorize @list
+  @list.update(list_params)
+end
 
-  def delete
-  end
+def delete
+end
 
-  private
+private
 
-  def list_params
+def list_params
   params.require(:list_params).permit(:name)
-  end
+end
 
-  def find_list
-  @list = List.find(params[:id])
+  def set_list
+    @list = List.find(params[:id])
   end
 end
