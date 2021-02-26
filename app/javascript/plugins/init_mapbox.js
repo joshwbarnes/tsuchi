e274c7c1df3ef1371f78778c62b1ad041e8a1917
Glenn Tippett <glenn.tippett@gmail.com>
2021-02-27 10:30:52 +1100

    add marker to map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,14 +10,16 @@ const initMapbox = (coordinates) => {
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
 
-    console.log(coordinates.lat);
-
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10',
       zoom: 13,
       center: [coordinates.long, coordinates.lat] // starting position [lng, lat]
     });
+
+    let marker = new mapboxgl.Marker()
+      .setLngLat([coordinates.long, coordinates.lat])
+      .addTo(map)
   }
 };
 
