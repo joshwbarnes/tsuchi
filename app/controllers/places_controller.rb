b28402c547c00f928621a73995ddaaadd8821816
Glenn Tippett <glenn.tippett@gmail.com>
2021-03-03 21:46:04 +1100

    increase radius of nearby places that show on the map

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 # This makes a call to the Google Places API
 class PlacesController < ApplicationController
   def call
-    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{params[:lat]},#{params[:long]}&radius=500&types=#{params[:category]}&key=#{ENV['PLACES_API_KEY']}"
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{params[:lat]},#{params[:long]}&radius=1000&types=#{params[:category]}&key=#{ENV['PLACES_API_KEY']}"
     response = RestClient.get(url)
     render json: response.body
   end
