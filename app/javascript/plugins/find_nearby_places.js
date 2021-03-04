9416803397a6c90c5cc34d7b81d981f83e4126ad
Glenn Tippett <glenn.tippett@gmail.com>
2021-03-04 20:33:04 +1100

    add icon

--- app/javascript/plugins/find_nearby_places.js
+++ app/javascript/plugins/find_nearby_places.js
@@ -1,16 +1,23 @@
 import { initMapbox } from './init_mapbox';
 
+// result.geometry.location.lat
+
 // Display names of nearby stores
 // to the view
 const displayNearbyStores = (results) => {
+  let icon;
   let storeNames = results.map((result) => {
+    let openIcon = '<i class="fas fa-eye"></i>';
+    let closedIcon = '<i class="fas fa-eye-slash"></i>';
+    icon = result.opening_hours.open_now ? openIcon : closedIcon;
     return result.name;
   });
 
+  // Display store name in HTML DOM
   const storeNameContainer = document.querySelector('.nearby-stores');
   storeNames.slice(-4).forEach((name) => {
     let element = `<p class="store-name">${name}</p>`;
-    storeNameContainer.insertAdjacentHTML('beforeend', element);
+    storeNameContainer.insertAdjacentHTML('afterbegin', element + icon);
   });
 }
 
