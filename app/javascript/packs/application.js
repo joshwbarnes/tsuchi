11d11d3dd0d8aefb44185310ae35e103543438a1
Glenn Tippett <glenn.tippett@gmail.com>
2021-03-09 20:16:05 +1100

    added notification

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,4 +13,21 @@ import { init_flatpickr } from '../plugins/init_flatpickr';
 document.addEventListener('turbolinks:load', () => {
   getLocation();
   init_flatpickr();
+
+  window.addEventListener("load", () => {
+  Notification.requestPermission()
+  .then((result) => {
+    console.log(result);
+  });
+});
+
+// Notification.permission - default, granted, denied
+
+// Placeholder icon:
+// https://picsum.photos/50
+
+
+var img = 'https://picsum.photos/50';
+var text = 'Check out this notification!';
+var notification = new Notification('Notification', { body: text, icon: img });
 });
