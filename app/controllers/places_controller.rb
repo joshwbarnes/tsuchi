2341fb6b63de70e6c722fb46136c61362c20869a
Glenn Tippett <glenn.tippett@gmail.com>
2021-03-13 17:38:11 +1100

    remove print console in rb file

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,6 @@ class PlacesController < ApplicationController
     domain_and_path = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
     query_string = "location=#{params[:lat]},#{params[:long]}&radius=#{radius}&types=#{params[:category]}&key=#{ENV['PLACES_API_KEY']}"
     url = "#{domain_and_path}?#{query_string}"
-    p url
     response = RestClient.get(url)
     render json: response.body
   end
