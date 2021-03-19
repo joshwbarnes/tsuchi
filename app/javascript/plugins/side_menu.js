c8f03ad4b9313e829194e8ac4ab114e9cfad8fbb
Glenn Tippett <glenn.tippett@gmail.com>
2021-03-19 22:07:18 +1100

    blur background elements when menu is open

--- app/javascript/plugins/side_menu.js
+++ app/javascript/plugins/side_menu.js
@@ -1,15 +1,27 @@
+// When the side menu is opened, this function blurs all 
+// Elements except the menu that is opened. 
+// When the menu is closed, they are set to normal
+const toggleBodyBlur = (blurValue) => {
+  const blur = document.querySelectorAll('.notification-modal, .notification-close, .container-item, div > *:not(section, div, button, span)');
+  blur.forEach((element) => {
+    element.style.filter = `blur(${blurValue}px)`;
+  });
+};
+
 const toggleSideMenu = () => {
   const menuModal = document.querySelector('.menu-modal');
   const burgerMenu = document.querySelector('.burger-menu');
   burgerMenu.addEventListener('click', () => {
     menuModal.style.transitionDuration = "0.5s";
     menuModal.style.right = '0';
-  });
-  
-  const closeMenu = document.querySelector('.close-menu');
-  closeMenu.addEventListener('click', () => {
-    menuModal.style.right = '-300px';
-  });
+    toggleBodyBlur(10);
+});
+
+const closeMenu = document.querySelector('.close-menu');
+closeMenu.addEventListener('click', () => {
+  menuModal.style.right = '-300px';
+  toggleBodyBlur(0);
+});
 };
 
 export { toggleSideMenu };
