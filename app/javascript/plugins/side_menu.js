a9f14c48f803dd7bef5bae42615afd3dcb7692f3
Glenn Tippett <glenn.tippett@gmail.com>
2021-03-20 00:48:27 +1100

    fic console log bug

--- app/javascript/plugins/side_menu.js
+++ app/javascript/plugins/side_menu.js
@@ -16,18 +16,20 @@ const toggleBodyBlur = (blurValue) => {
 const toggleSideMenu = () => {
   const menuModal = document.querySelector('.menu-modal');
   const burgerMenu = document.querySelector('.burger-menu');
-  burgerMenu.addEventListener('click', () => {
-    menuModal.style.transitionDuration = "0.5s";
-    menuModal.style.right = '0';
-    toggleBodyBlur(10);
-  });
-  // Move the side menu off screen when user clicks
-  // the close button
-  const closeMenu = document.querySelector('.close-menu');
-  closeMenu.addEventListener('click', () => {
-    menuModal.style.right = '-300px';
-    toggleBodyBlur(0);
-  });
+  if (menuModal) {
+    burgerMenu.addEventListener('click', () => {
+      menuModal.style.transitionDuration = "0.5s";
+      menuModal.style.right = '0';
+      toggleBodyBlur(10);
+    });
+    // Move the side menu off screen when user clicks
+    // the close button
+    const closeMenu = document.querySelector('.close-menu');
+    closeMenu.addEventListener('click', () => {
+      menuModal.style.right = '-300px';
+      toggleBodyBlur(0);
+    });
+  }
 };
 
 export { toggleSideMenu };
